7c3206a30d3004581332de55dcd0348095f067f1
Aqsin <heyderliaqsin94@gmail.com>
2022-01-17 10:42:08 +0400

    Product Manager

---
+++ Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Product:BaseEntity
+    {
+        [MaxLength(100)]
+        public string name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ushort InStock { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Discount { get; set; }
+        public string SKU { get; set; }
+        public DateTime  PublishDate { get; set; }
+        public string Barcode { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+
+    }
+}
---
+++ Services/ProductManager.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductManager
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Product> GetAll()
+        {
+            return _context.Products.Where(x=>!x.IsDeleted).OrderByDescending(x=>x.PublishDate).ToList();
+        }
+
+        public void Add(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+        }
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            _context.SaveChanges();
+
+        }
+        public void Delete(Product product)
+        {
+            product.IsDeleted = true;
+            _context.SaveChanges();
+
+        }
+    }
+}
